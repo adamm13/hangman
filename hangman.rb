82f3271929deb2ae2a64528ef0c6f216496d7ac8
Adam Mohammed <13.adamm@gmail.com>
2021-05-27 15:42:06 -0400

    added words and clues

--- hangman.rb
+++ hangman.rb
@@ -3,6 +3,17 @@ class Hangman
 
     def initialize
       letters = ('a'..'z').to_a
+      word = words.sample
+    end
+
+    def words
+      [
+        ["cricket", "A game played by gentlemen.."],
+        ["jogging", "Not walking is called.."],
+        ["celebrate", "What you do on your birthday.."],
+        ["computer", "What are you playing this on.."],
+        ["exotic", "Not from around here"],
+      ]
     end
 
     def begin
