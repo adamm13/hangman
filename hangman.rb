bc64e5dff915e8161a69009f4440ec46d7846ac2
Adam Mohammed <13.adamm@gmail.com>
2021-05-27 16:09:30 -0400

    added lives and bad guess displays

--- hangman.rb
+++ hangman.rb
@@ -5,6 +5,7 @@ class Hangman
       #added @ to make instance variable to use in other places.
       @letters = ('a'..'z').to_a
       @word = words.sample
+      @lives = 7
     end
 
     def words
@@ -17,20 +18,43 @@ class Hangman
       ]
     end
 
-    def begin
-      #ask user for a letter
-      puts "New game has started...Your word is #{words.last.size} characters long"
+    def print_teaser
       word_teaser = ""
 
       @word.first.size.times do
         word_teaser += "_ "
       end
 
-      puts
+      puts word_teaser
+    end
 
-      puts "Clue: #{@word.last}"
+    def make_guess
+      if @lives > 0
       puts "Enter a Letter"
       guess = gets.chomp
+
+      #if letters is not part of the word remove from the array
+      good_guess = @word.first.include? guess
+
+      if good_guess
+        puts "You are correct!"
+      else
+        @lives -= 1
+        puts "Sorry ...you have #{@lives} lives remaining. Try again"
+        make_guess
+      end
+    else puts "Game over, Better luck next time"
+    end
+    end
+
+    def begin
+      #ask user for a letter
+      puts "New game has started...Your word is #{words.last.size} characters long"
+      print_teaser
+
+      puts "Clue: #{@word.last}"
+
+      make_guess
 
       puts "You Guessed #{guess}"
     end
